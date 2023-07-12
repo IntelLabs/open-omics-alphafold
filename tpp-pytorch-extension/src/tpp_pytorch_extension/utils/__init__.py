1f4a4472f06f19852845b629233fcd0a3f6bdd11
yangwei <yangwei>
2023-07-12 14:31:55 +0800

    add submodule link to TPP

--- tpp-pytorch-extension/src/tpp_pytorch_extension/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# Copyright (c) 2022 Intel Corporation - All rights reserved.                 #
-#                                                                             #
-# For information on the license, see the LICENSE file.                       #
-# Further information: https://github.com/libxsmm/tpp-pytorch-extension/      #
-# SPDX-License-Identifier: BSD-3-Clause                                       #
-###############################################################################
-# Author: Dhiraj Kalamkar (Intel Corp.)                                       #
-###############################################################################
